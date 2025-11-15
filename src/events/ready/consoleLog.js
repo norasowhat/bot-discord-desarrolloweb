9899d20f7df3a497aaea15e4ee5b2f255718f999
Noraaa <0268966@up.edu.mx>
2025-11-14 21:57:22 -0600

    cambios

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -2,16 +2,16 @@ const { ActivityType } = require("discord.js");
 
 let status = [
   {
-    name: "Pirro",
+    name: "En vivo",
     type: ActivityType.Streaming,
     url: "https://www.youtube.com/watch?v=BME88lS6aVY&list=RDBME88lS6aVY&start_radio=1",
   },
   {
-    name: "Pirro",
+    name: "Reyes de las olas",
     type: ActivityType.Watching,
   },
   {
-    name: "Pirro",
+    name: "Woods, Mac Miller",
     type: ActivityType.Listening,
   },
 ];
